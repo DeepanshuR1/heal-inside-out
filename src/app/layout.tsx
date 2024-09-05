5ec4749966337e04589d92c39c20ce121941a4f2
Deepanshu <Deepanshur1@verifone.com>
2024-09-05 21:19:16 +0530

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="image.png" />
+      </head>
       <body className="relative">
         <NavBar />
         <div className="whatsapp-button">
